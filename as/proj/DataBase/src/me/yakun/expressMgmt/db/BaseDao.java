cb998ac87ade62d3c160c37b6de0475e88f98c2d
chenyakun <chenyakun@foxmail.com>
2013-01-06 22:46:18 +0800

    new framework

---
+++ as/proj/DataBase/src/me/yakun/expressMgmt/db/BaseDao.java
@@ -0,0 +1,19 @@
+/**    
+ * @FileName: BaseDao.java  
+ * @Package:me.yakun.expressMgmt.db  
+ * @Description: TODO 
+ * @author: ME   
+ * @date:2013-1-6 下午9:53:20  
+ * @version V1.0    
+ */ 
+package me.yakun.expressMgmt.db; 
+
+/** 
+ * @ClassName: BaseDao  
+ * @Description: TODO 
+ * @author: ME 
+ * @date:2013-1-6 下午9:53:20   
+ */
+public class BaseDao {
+
+}
---
+++ as/proj/DataBase/src/me/yakun/expressMgmt/db/model/UserInfo.java
@@ -0,0 +1,21 @@
+/**    
+ * @FileName: UserInfo.java  
+ * @Package:me.yakun.expressMgmt.db.model  
+ * @Description: TODO 
+ * @author: ME   
+ * @date:2013-1-6 下午9:58:37  
+ * @version V1.0    
+ */ 
+package me.yakun.expressMgmt.db.model; 
+
+/** 
+ * @ClassName: UserInfo  
+ * @Description: TODO 
+ * @author: ME 
+ * @date:2013-1-6 下午9:58:37   
+ */
+public class UserInfo {
+
+	private String userid;
+	private String username; 
+}
