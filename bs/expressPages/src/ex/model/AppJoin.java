de8f7820dba4731304d03f31a960089fe43794c6
chenyakun <chenyakun@foxmail.com>
2013-05-02 22:40:43 +0800

    稍作修改 无其他改动

--- bs/expressPages/src/ex/model/AppJoin.java
+++ bs/expressPages/src/ex/model/AppJoin.java
@@ -20,69 +20,87 @@ public class AppJoin implements java.io.Serializable {
 	private String mobile;
 	private String password;
 	private String imgCode;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getExpressName() {
 		return expressName;
 	}
+
 	public void setExpressName(String expressName) {
 		this.expressName = expressName;
 	}
+
 	public String getExpressCode() {
 		return expressCode;
 	}
+
 	public void setExpressCode(String expressCode) {
 		this.expressCode = expressCode;
 	}
+
 	public String getNetworkName() {
 		return networkName;
 	}
+
 	public void setNetworkName(String networkName) {
 		this.networkName = networkName;
 	}
+
 	public String getStartPlace() {
 		return startPlace;
 	}
+
 	public void setStartPlace(String startPlace) {
 		this.startPlace = startPlace;
 	}
+
 	public String getStartPlace_input() {
 		return startPlace_input;
 	}
+
 	public void setStartPlace_input(String startPlace_input) {
 		this.startPlace_input = startPlace_input;
 	}
+
 	public String getHeadName() {
 		return headName;
 	}
+
 	public void setHeadName(String headName) {
 		this.headName = headName;
 	}
+
 	public String getMobile() {
 		return mobile;
 	}
+
 	public void setMobile(String mobile) {
 		this.mobile = mobile;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getImgCode() {
 		return imgCode;
 	}
+
 	public void setImgCode(String imgCode) {
 		this.imgCode = imgCode;
 	}
-
 
 }
